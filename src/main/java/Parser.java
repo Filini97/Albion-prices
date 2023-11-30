0b2d8ed906bc0a0e164a71771d43d6e51db9fb8c
Alex-Filini <Filini97@yandex.ru>
2023-11-30 16:20:06 +0300

    getPage implemented.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,12 +1,19 @@
-import javax.swing.text.Document;
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Parser {
 
-    private static Document getPage() {
-        return null;
+    private static Document getPage() throws IOException {
+        final String URL = "https://albiononline2d.com/en/item";
+        Document page = Jsoup.parse(new URL(URL), 2000);
+        return page;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println(getPage());
     }
 
 }

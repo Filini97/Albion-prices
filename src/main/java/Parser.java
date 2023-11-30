749a5ea3114f44d0fb046a9f842391b89e280466
Alex-Filini <Filini97@yandex.ru>
2023-11-30 16:15:46 +0300

    Initial commit

---
+++ src/main/java/Parser.java
@@ -0,0 +1,12 @@
+import javax.swing.text.Document;
+
+public class Parser {
+
+    private static Document getPage() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
